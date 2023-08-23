2218e1511079e97367ed8947026953c7a993feea
Andre Lucas <andrelucca99@gmail.com>
2023-08-23 09:35:35 -0300

    refactor: Corrigido erro do dropdown-uf

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,28 +1,40 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 import { EstadosBrService } from 'src/app/core/services/estados-br.service';
 import { EstadosBr } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
-  styleUrls: ['./dropdown-uf.component.scss']
+  styleUrls: ['./dropdown-uf.component.scss'],
 })
 export class DropdownUfComponent implements OnInit {
   @Input() label: string = '';
   @Input() iconePrefixo: string = '';
+  @Input() control!: FormControl;
 
   estadosBr: EstadosBr[] = [];
+  filteredOptions$?: Observable<EstadosBr[]>;
 
-  options = [];
+  constructor(private estadosBrService: EstadosBrService) {}
 
-  constructor ( private estadosBrService: EstadosBrService ) {
-
-  }
   ngOnInit(): void {
-    this.estadosBrService.listar()
-      .subscribe(dados => {
-        this.estadosBr = dados
-        console.log(this.estadosBr)
-      })
+    this.estadosBrService.listar().subscribe((dados) => {
+      this.estadosBr = dados;
+      console.log(this.estadosBr);
+    });
+    this.filteredOptions$ = this.control.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.filtrarUfs(value))
+    );
+  }
+
+  filtrarUfs(value: string): EstadosBr[] {
+    const valorFiltrado = value?.toLowerCase();
+    const result = this.estadosBr.filter((estado) =>
+      estado.nome.toLowerCase().includes(valorFiltrado)
+    );
+    return result;
   }
 }
