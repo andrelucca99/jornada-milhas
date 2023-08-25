65aa08cf151460b5c521c02a4be5513591b068a9
Andre Lucas <andrelucca99@gmail.com>
2023-08-25 13:56:40 -0300

    feat: controlando formulario reativo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private formBuild: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuild.group({
+      email: [null],
+      senha: [null],
+    })
+  }
+
+  login() {
+    console.log('login', this.loginForm.value)
+  }
 }
