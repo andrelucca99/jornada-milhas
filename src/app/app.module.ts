b9e3eb2c4d8098394f332692cbd7d1f2a77a8f6f
Andre Lucas <andrelucca99@gmail.com>
2023-08-29 15:47:11 -0300

    feat: add formulario base para usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,10 @@ import { DropdownUfComponent } from './shared/form-busca/dropdown-uf/dropdown-uf
 import { DepoimentosComponent } from './shared/depoimentos/depoimentos.component';
 import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-passageiro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { FormUserComponent } from './shared/form-user/form-user.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -54,7 +58,8 @@ import { LoginComponent } from './pages/login/login.component';
     DropdownUfComponent,
     DepoimentosComponent,
     SeletorPassageiroComponent,
-    LoginComponent
+    LoginComponent,
+    FormUserComponent
   ],
   imports: [
     BrowserModule,
@@ -73,7 +78,10 @@ import { LoginComponent } from './pages/login/login.component';
     MatDialogModule,
     HttpClientModule,
     ReactiveFormsModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatDividerModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/form-user/form-user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { EstadosBr } from 'src/app/core/types/type';
+
+@Component({
+  selector: 'app-form-user',
+  templateUrl: './form-user.component.html',
+  styleUrls: ['./form-user.component.scss']
+})
+export class FormUserComponent implements OnInit {
+  cadastroForm!: FormGroup;
+  estadoControl = new FormControl<EstadosBr | null>(null, Validators.required);
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cadastroForm = this.formBuilder.group({
+      nome: [null, Validators.required],
+      nascimento: [null, Validators.required],
+      cpf: [null, Validators.required],
+      cidade: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, [Validators.required, Validators.minLength(3)]],
+      genero: ['outro'],
+      telefone: [null, Validators.required],
+      estado: this.estadoControl,
+      confirmarEmail: [null, [Validators.required, Validators.email]],
+      confirmarSenha: [null, [Validators.required, Validators.minLength(3)]],
+      aceitarTermos: [null, Validators.requiredTrue]
+    });
+  }
+}
