6c3febccf7442e289bad92e75f81346c529e1b16
Andre Lucas <andrelucca99@gmail.com>
2023-08-31 14:55:57 -0300

    feat: add metodos para atualizar e deslogar do perfil do usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CadastroService } from 'src/app/core/services/cadastro.service';
 import { FormularioService } from 'src/app/core/services/formulario.service';
 import { TokenService } from 'src/app/core/services/token.service';
+import { UserService } from 'src/app/core/services/user.service';
 import { PessoaUsuaria } from 'src/app/core/types/type';
 
 @Component({
@@ -23,7 +25,9 @@ export class PerfilComponent implements OnInit {
   constructor(
     private tokenService: TokenService,
     private cadastroService: CadastroService,
-    private formularioService: FormularioService
+    private formularioService: FormularioService,
+    private router: Router,
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
@@ -51,10 +55,31 @@ export class PerfilComponent implements OnInit {
   }
 
   deslogar() {
-    console.log('Logout realizado com sucesso');
+    this.userService.logout();
+    this.router.navigate(['/login']);
   }
 
   atualizar() {
-    console.log('Cadastro atualizado com sucesso');
+    const dadosAtualizados = {
+      nome: this.form?.value.nome,
+      nascimento: this.form?.value.nascimento,
+      cpf: this.form?.value.cpf,
+      telefone: this.form?.value.telefone,
+      email: this.form?.value.email,
+      senha: this.form?.value.senha,
+      genero: this.form?.value.genero,
+      cidade: this.form?.value.cidade,
+      estado: this.form?.value.estado
+    }
+
+    this.cadastroService.editarCadastro(dadosAtualizados, this.token).subscribe({
+      next: () => {
+        alert('Cadastro editado com sucesso');
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.log('Erro ao atualizar cadastro', err);
+      }
+    })
   }
 }
