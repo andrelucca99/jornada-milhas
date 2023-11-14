f1ef87669a16b6bce59ea1bb4f921f2c73a773a3
Andre Lucas <andrelucca99@gmail.com>
2023-11-14 11:50:14 -0300

    feat: add modulo home

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { HomeComponent } from "./home.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import { MaterialModule } from "src/app/core/material/material.module";
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    SharedModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+
+export class HomeModule { }
